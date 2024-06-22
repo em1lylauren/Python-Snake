2ca1dc0ed6b5e6fd1d6e3e390157ab146790c7f7
EmilyH <emlhk080804@gmail.com>
2024-06-22 14:48:27 -0600

    - Added window size, icon, and title

--- Snake.py
+++ Snake.py
@@ -1,1 +1,17 @@
-import pygame
+import pygame as py
+import time as t
+import random as rand
+
+# Window size
+windowXSize = 600
+windowYSize = 800
+
+# Initialization
+py.init()
+screen = py.display.set_mode((windowXSize, windowYSize))
+py.display.set_caption("Snake")
+
+icon = py.image.load("apple.png")
+py.display.set_icon(icon)
+
+
